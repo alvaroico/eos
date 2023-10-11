7b71d02cb77c132125826cd2cdc57559055f4e63
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2023-10-11 19:25:40 -0300

    Swagger install

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('EOS API')
+    .setDescription('Teste prático para desenvolvedores Back-End')
+    .setVersion('0.0.1')
+    .addBearerAuth()
+    .addTag('EOS')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('Swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
