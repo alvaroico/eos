78707d79680397fd07e55f1cb8afbd064501a52e
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2023-10-15 13:39:49 -0300

    New Post

--- src/entity/Comment/comment.controller.ts
+++ src/entity/Comment/comment.controller.ts
@@ -1,13 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { Comment } from './comment.entity';
 import { CommentService } from './comment.service';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from 'src/guard/auth.guard';
+import { ITokenJWT } from 'src/guard/auth.guard.interface';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 
+@ApiBearerAuth()
 @Controller('/comment')
+@UseGuards(AuthGuard)
 export class CommentController {
-  constructor(private readonly commentService: CommentService) {}
+  constructor(
+    private readonly commentService: CommentService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   @Get()
   async getAll(): Promise<Comment[]> {
     return await this.commentService.findAll();
+  }
+
+  @Post()
+  async create(
+    @Body() body: { comment: Comment },
+    @Req() request: Request,
+  ): Promise<Comment> {
+    return await this.commentService.create(
+      body.comment,
+      this.jwtService.decode(
+        request.headers.authorization.split(' ')[1],
+      ) as ITokenJWT,
+    );
   }
 }
--- src/entity/Comment/comment.service.ts
+++ src/entity/Comment/comment.service.ts
@@ -1,14 +1,28 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Comment } from './comment.entity';
+import { ITokenJWT } from 'src/guard/auth.guard.interface';
+import { Post } from '../Post/post.entity';
 
 @Injectable()
 export class CommentService {
   constructor(
     @Inject('COMMENT_REPOSITORY')
     private commentRepository: Repository<Comment>,
+    @Inject('POST_REPOSITORY')
+    private postRepository: Repository<Post>,
   ) {}
   async findAll(): Promise<Comment[]> {
     return this.commentRepository.find();
+  }
+  async create(comment: Comment, token: ITokenJWT): Promise<Comment> {
+    const post = await this.postRepository.findOne({
+      where: { id: comment.post_id },
+    });
+    if (!post) {
+      throw new HttpException('Post não encontrado', HttpStatus.NOT_FOUND);
+    }
+    comment.user_id = token.id;
+    return this.commentRepository.save(comment);
   }
 }
